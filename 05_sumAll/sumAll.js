d59ae71ca38b3cc37962e6782f0f3bde84ac2583
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2023-09-30 12:48:27 -0500

    exercise 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(firstNumber,secondNumber) {
+    if(firstNumber<0 || secondNumber<0) return 'ERROR'
+    if(typeof(firstNumber) !== "number" || typeof(secondNumber) !== "number") return 'ERROR'
+    
+    let a = 0
+    let b = 0
 
+    if (firstNumber > secondNumber) {
+        a = firstNumber
+        b = secondNumber
+    }
+
+    else {
+        a = secondNumber
+        b = firstNumber
+    }
+    
+    let answer = 0
+    for(let i=b; i<=a; i++) {
+        answer = answer+i
+    }
+
+    return answer
 };
 
 // Do not edit below this line
