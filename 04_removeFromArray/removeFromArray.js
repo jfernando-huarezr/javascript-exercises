0bcb67f284b5b88998ddca83d7f1e53dc46434f7
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2023-09-30 12:37:33 -0500

    exercise 4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+    let workArr = [...arr]
+    let filtered = []
 
+    for (let i=0; i<values.length; i++) {
+        if (workArr.includes(values[i])){
+
+            console.log(values[i])
+
+            workArr = workArr.filter(element => {
+                return element !== values[i]
+            }) 
+        }
+    }
+    return workArr
 };
 
 // Do not edit below this line
