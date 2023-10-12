989bdfed9eb456208741c9de4f6bde130abece90
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2023-10-11 23:34:02 -0500

    calculator completed!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,17 +18,25 @@ const sum = function(arr) {
   return answer
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  if (!arr.length) return 0
+  
 
+  const answer = arr.reduce((totalMul, element) => {
+    return totalMul*element
+  }, 1)
+
+  return answer
 };
 
 
-const power = function() {
-	
+const power = function(num1,num2) {
+  return Math.pow(num1,num2)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0) return 1
+  return factorial(num-1)*num
 };
 
 // Do not edit below this line
